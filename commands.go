1c55fb42634253c4933959b10489c93ca5d7df1d
Matthew Rankin <matthew@cumulusware.com>
2020-01-13 10:21:25 -0600

    Moved some methods to separate file.

---
+++ commands.go
@@ -0,0 +1,223 @@
+// Copyright (c) 2020 The prologix developers. All rights reserved.
+// Project site: https://github.com/gotmc/prologix
+// Use of this source code is governed by a MIT-style license that
+// can be found in the LICENSE.txt file for the project.
+
+package prologix
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// AssertEOI determines if the Prologix controller is configured to assert the
+// EOI signal at the end of any command sent over the GPIB port.
+func (c *Controller) AssertEOI() (bool, error) {
+	s, err := c.QueryCommand("eoi")
+	if err != nil {
+		return false, err
+	}
+	var eoi bool
+	if strings.TrimSpace(s) == "1" {
+		eoi = true
+	} else if strings.TrimSpace(s) == "0" {
+		eoi = false
+	} else {
+		return false, fmt.Errorf("eoi mode not determinable; received %s", s)
+	}
+	if eoi != c.eoi {
+		c.eoi = eoi
+		return false, fmt.Errorf("internal state mismatch, eoi is now %t", eoi)
+	}
+	return eoi, nil
+}
+
+// ClearDevice sends the `clr` command to the Prologix controller which sends
+// the Selected Device Clear (SDC) message to the currently selected GPIB
+// address.
+func (c *Controller) ClearDevice() error {
+	return c.Command("clr")
+}
+
+// ClearInterface sends the `ifc` command to the Prologix controller which
+// asserts the GPIB Interface Clear (IFC) signal for 150 microseconds making
+// the Prologix GPIB controller the Controller-In-Charge.
+func (c *Controller) ClearInterface() error {
+	return c.Command("ifc")
+}
+
+// FrontPanel enables or disables front panel operation for the instrument at
+// the current GPIB address. This is accomplished by either sending the
+// Prologix `loc` local command to enable the front panel or by sending the
+// Prologix `llo` local lockout command to disable the front panel.
+func (c *Controller) FrontPanel(enable bool) error {
+	cmd := "llo" // Local lockout
+	if enable {
+		cmd = "loc" // Local
+	}
+	return c.Command(cmd)
+}
+
+// GPIBTermination uses the Prologix `eos` command to query the GPIB
+// terminator.
+func (c *Controller) GPIBTermination() (GpibTerm, error) {
+	s, err := c.QueryCommand("eos")
+	if err != nil {
+		return 0, err
+	}
+	term, err := strconv.Atoi(strings.TrimSpace(s))
+	if err != nil {
+		return 0, err
+	}
+	return GpibTerm(term), nil
+}
+
+// InstrumentAddress returns the GPIB address for the instrument under control.
+func (c *Controller) InstrumentAddress() (int, error) {
+	// FIXME(mdr): Need to update so that it can handle a secondary address.
+	s, err := c.QueryCommand("addr")
+	if err != nil {
+		return 0, err
+	}
+	i, err := strconv.ParseInt(strings.TrimSpace(s), 10, 32)
+	if err != nil {
+		return 0, err
+	}
+	addr := int(i)
+	if addr != c.instrumentAddr {
+		c.instrumentAddr = addr
+		return addr, fmt.Errorf("internal state mismatch, address is now %d", addr)
+	}
+	return addr, nil
+}
+
+// ReadAfterWrite determines if the Prologix controller is configured to
+// automatically read after a write.
+func (c *Controller) ReadAfterWrite() (bool, error) {
+	s, err := c.QueryCommand("auto")
+	if err != nil {
+		return false, err
+	}
+	var auto bool
+	if strings.TrimSpace(s) == "1" {
+		auto = true
+	} else if strings.TrimSpace(s) == "0" {
+		auto = false
+	} else {
+		return false, fmt.Errorf("auto mode not determinable; received %s", s)
+	}
+	if auto != c.auto {
+		c.auto = auto
+		return false, fmt.Errorf("internal state mismatch, auto is now %t", auto)
+	}
+	return auto, nil
+}
+
+// ReadTimeout queries the read timeout value in milliseconds from the Prologix
+// GPIB controller.
+func (c *Controller) ReadTimeout() (int, error) {
+	s, err := c.QueryCommand("read_tmo_ms")
+	if err != nil {
+		return 0, err
+	}
+	i, err := strconv.ParseInt(strings.TrimSpace(s), 10, 32)
+	if err != nil {
+		return 0, err
+	}
+	readTimeout := int(i)
+	if readTimeout < 1 || readTimeout > 3000 {
+		return 0, fmt.Errorf("read timeout must be between 1 and 3000 ms was set to %d", readTimeout)
+	}
+	return readTimeout, nil
+}
+
+// Reset performs a power-on reset of the controller. The process takes about 5
+// seconds. All input received during this time are ignored.
+func (c *Controller) Reset() error {
+	return c.Command("rst")
+}
+
+// ServiceRequest sends the `srq` command to the Prologix controller to
+// determine if the GPIB SRQ signal is asserted or not.
+func (c *Controller) ServiceRequest() (bool, error) {
+	s, err := c.QueryCommand("srq")
+	if err != nil {
+		return false, err
+	}
+	var srq bool
+	if strings.TrimSpace(s) == "1" {
+		srq = true
+	} else if strings.TrimSpace(s) == "0" {
+		srq = false
+	} else {
+		return false, fmt.Errorf("srq not determinable; received %s", s)
+	}
+	return srq, nil
+}
+
+// SetAssertEOI sets the Prologix controller to assert the EOI signal after the
+// last character of a command sent over the GPIB port.
+func (c *Controller) SetAssertEOI(enable bool) error {
+	cmd := "eoi 0"
+	if enable {
+		cmd = "eoi 1"
+	}
+	err := c.Command(cmd)
+	// As long as there wasn't an error setting the eoi mode on the Prologix
+	// controller, set the eoi status in the controller struct.
+	if err != nil {
+		return err
+	}
+	c.eoi = enable
+	return nil
+}
+
+// SetGPIBTermination uses the Prologix `eos` command to set the character to
+// be appended as the GPIB terminator to all data sent from the Prologix
+// Controller to the instrument.
+func (c *Controller) SetGPIBTermination(term GpibTerm) error {
+	return c.Command(fmt.Sprintf("eos %d", term))
+}
+
+// SetInstrumentAddress sets the GPIB address for the instrument under control.
+func (c *Controller) SetInstrumentAddress(addr int) error {
+	cmd := fmt.Sprintf("addr %d", addr)
+	err := c.Command(cmd)
+	if err != nil {
+		return err
+	}
+	c.instrumentAddr = addr
+	return nil
+}
+
+// SetReadAfterWrite sets the Proglogix controller to automatically read after write.
+func (c *Controller) SetReadAfterWrite(enable bool) error {
+	// Send the proper command based on whether enabling or disabling.
+	cmd := "auto 0"
+	if enable {
+		cmd = "auto 1"
+	}
+	err := c.Command(cmd)
+	// As long as there wasn't an error setting the auto mode on the Prologix
+	// controller, set the auto status in the controller struct.
+	if err != nil {
+		return err
+	}
+	c.auto = enable
+	return nil
+}
+
+// SetReadTimeout sets the Proglogix controller's read timeout in milliseconds.
+// The timeout must be between 1 and 3000 milliseconds.
+func (c *Controller) SetReadTimeout(timeout int) error {
+	if timeout < 1 || timeout > 3000 {
+		return fmt.Errorf("read timeout outside 1 to 3000 ms; attempted to set to %d", timeout)
+	}
+	return c.Command(fmt.Sprintf("read_tmo_ms %d", timeout))
+}
+
+// Version returns the version string from the Prologix GPIB controller.
+func (c *Controller) Version() (string, error) {
+	return c.QueryCommand("ver")
+}
--- controller.go
+++ controller.go
@@ -9,7 +9,6 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"strconv"
 	"strings"
 )
 
@@ -121,181 +120,24 @@ func (c *Controller) Command(cmd string) error {
 	return err
 }
 
-// InstrumentAddress returns the GPIB address for the instrument under control.
-func (c *Controller) InstrumentAddress() (int, error) {
-	// FIXME(mdr): Need to update so that it can handle a secondary address.
-	s, err := c.QueryCommand("addr")
-	if err != nil {
-		return 0, err
-	}
-	i, err := strconv.ParseInt(strings.TrimSpace(s), 10, 32)
-	if err != nil {
-		return 0, err
-	}
-	addr := int(i)
-	if addr != c.instrumentAddr {
-		c.instrumentAddr = addr
-		return addr, fmt.Errorf("internal state mismatch, address is now %d", addr)
-	}
-	return addr, nil
+// GpibTerm provides the type for the available GPIB terminators.
+type GpibTerm int
+
+// Available GPIB terminators for the Prologix Controller.
+const (
+	AppendCRLF GpibTerm = iota
+	AppendCR
+	AppendLF
+	AppendNothing
+)
+
+var gpibTermDesc = map[GpibTerm]string{
+	AppendCRLF:    `Append CR+LF (\r\n) to instrument commands`,
+	AppendCR:      `Append CR (\r) to instrument commands`,
+	AppendLF:      `Append LF (\n) to instrument commands`,
+	AppendNothing: `Do not append anything to instrument commands`,
 }
 
-// SetInstrumentAddress sets the GPIB address for the instrument under control.
-func (c *Controller) SetInstrumentAddress(addr int) error {
-	cmd := fmt.Sprintf("addr %d", addr)
-	err := c.Command(cmd)
-	if err != nil {
-		return err
-	}
-	c.instrumentAddr = addr
-	return nil
-}
-
-// ReadAfterWrite determines if the Prologix controller is configured to
-// automatically read after a write.
-func (c *Controller) ReadAfterWrite() (bool, error) {
-	s, err := c.QueryCommand("auto")
-	if err != nil {
-		return false, err
-	}
-	var auto bool
-	if strings.TrimSpace(s) == "1" {
-		auto = true
-	} else if strings.TrimSpace(s) == "0" {
-		auto = false
-	} else {
-		return false, fmt.Errorf("auto mode not determinable; received %s", s)
-	}
-	if auto != c.auto {
-		c.auto = auto
-		return false, fmt.Errorf("internal state mismatch, auto is now %t", auto)
-	}
-	return auto, nil
-}
-
-// SetReadAfterWrite sets the Proglogix controller to automatically read after write.
-func (c *Controller) SetReadAfterWrite(enable bool) error {
-	// Send the proper command based on whether enabling or disabling.
-	cmd := "auto 0"
-	if enable {
-		cmd = "auto 1"
-	}
-	err := c.Command(cmd)
-	// As long as there wasn't an error setting the auto mode on the Prologix
-	// controller, set the auto status in the controller struct.
-	if err != nil {
-		return err
-	}
-	c.auto = enable
-	return nil
-}
-
-// SendEOI determines if the Prologix controller is configured to append the
-// EOI signal to the end of any command sent over the GPIB port.
-func (c *Controller) SendEOI() (bool, error) {
-	s, err := c.QueryCommand("eoi")
-	if err != nil {
-		return false, err
-	}
-	var eoi bool
-	if strings.TrimSpace(s) == "1" {
-		eoi = true
-	} else if strings.TrimSpace(s) == "0" {
-		eoi = false
-	} else {
-		return false, fmt.Errorf("eoi mode not determinable; received %s", s)
-	}
-	if eoi != c.eoi {
-		c.eoi = eoi
-		return false, fmt.Errorf("internal state mismatch, eoi is now %t", eoi)
-	}
-	return eoi, nil
-}
-
-// SetSendEOI sets the Proglogix controller to append the EOI signal after the
-// last character of a command sent over the GPIB port.
-func (c *Controller) SetSendEOI(enable bool) error {
-	cmd := "eoi 0"
-	if enable {
-		cmd = "eoi 1"
-	}
-	err := c.Command(cmd)
-	// As long as there wasn't an error setting the eoi mode on the Prologix
-	// controller, set the eoi status in the controller struct.
-	if err != nil {
-		return err
-	}
-	c.eoi = enable
-	return nil
-}
-
-// ReadTimeout queries the read timeout value in milliseconds from the Prologix
-// GPIB controller.
-func (c *Controller) ReadTimeout() (int, error) {
-	s, err := c.QueryCommand("read_tmo_ms")
-	if err != nil {
-		return 0, err
-	}
-	i, err := strconv.ParseInt(strings.TrimSpace(s), 10, 32)
-	if err != nil {
-		return 0, err
-	}
-	readTimeout := int(i)
-	if readTimeout < 1 || readTimeout > 3000 {
-		return 0, fmt.Errorf("read timeout must be between 1 and 3000 ms was set to %d", readTimeout)
-	}
-	return readTimeout, nil
-}
-
-// SetReadTimeout sets the Proglogix controllers read timeout in milliseconds.
-// The timeout must be between 1 and 3000 milliseconds.
-func (c *Controller) SetReadTimeout(timeout int) error {
-	if timeout < 1 || timeout > 3000 {
-		return fmt.Errorf("read timeout outside 1 to 3000 ms; attempted to set to %d", timeout)
-	}
-	return c.Command(fmt.Sprintf("read_tmo_ms %d", timeout))
-}
-
-// FrontPanel enables (local mode) or disables (local lockout) front panel
-// operation for the instrument at the current GPIB address.
-func (c *Controller) FrontPanel(enable bool) error {
-	cmd := "llo" // Local lockout
-	if enable {
-		cmd = "loc" // Local
-	}
-	return c.Command(cmd)
-}
-
-// InterfaceClear asserts the GPIB IFC singal for 150 microseconds making the
-// Prologix GPIB controller the Controller-In-Charge on the GPIB.
-func (c *Controller) InterfaceClear() error {
-	return c.Command("ifc")
-}
-
-// ServiceRequest determines if the GPIB SRQ signal is asserted or not.
-func (c *Controller) ServiceRequest() (bool, error) {
-	s, err := c.QueryCommand("srq")
-	if err != nil {
-		return false, err
-	}
-	var srq bool
-	if strings.TrimSpace(s) == "1" {
-		srq = true
-	} else if strings.TrimSpace(s) == "0" {
-		srq = false
-	} else {
-		return false, fmt.Errorf("srq not determinable; received %s", s)
-	}
-	return srq, nil
-}
-
-// Version returns the version string from the Prologix GPIB-USB controller.
-func (c *Controller) Version() (string, error) {
-	return c.QueryCommand("ver")
-}
-
-// Clear sends the Selected Device Clear (SDC) message to the currently
-// selected GPIB address.
-func (c *Controller) Clear() error {
-	return c.Command("clr")
+func (term GpibTerm) String() string {
+	return gpibTermDesc[term]
 }
